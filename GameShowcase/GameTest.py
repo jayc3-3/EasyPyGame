3a6b5b73a863f576297079a531290890ac40fab0
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-02-27 18:06:46 -0800

    Update GameTest.py

--- GameShowcase/GameTest.py
+++ GameShowcase/GameTest.py
@@ -8,6 +8,10 @@ PyGame.CreateObject('Player.png')
 while PyGame.IsRunning:
     PyGame.FillScreen((200, 200, 200))
     
+    ShowcaseText = PyGame.CreateText("Made with EasyPyGame!", 'Arial.ttf', 50, True, (0, 0, 0))
+    
+    FPSText = PyGame.CreateText("FPS: " + str(PyGame.FPS()), 'Arial.ttf', 25, True, (0, 0, 0))
+    
     if PyGame.KeyPressed('Up'):
         PyGame.Objects[0].Y -= int(325 * PyGame.DeltaTime)
 
@@ -23,5 +27,8 @@ while PyGame.IsRunning:
     for Object in PyGame.Objects:
         Object.Rect = Object.Image.get_rect(topleft=(Object.X, Object.Y))
         PyGame.DrawToScreen(Object.Image, Object.Rect)
+    
+    PyGame.DrawToScreen(ShowcaseText, (100, 100))
+    PyGame.DrawToScreen(FPSText, (25, 25))
     
     PyGame.Update()
