25f735816dbc92c22a5191b1f63e54886379dd34
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-02-27 17:58:31 -0800

    Update GameTest.py

--- GameShowcase/GameTest.py
+++ GameShowcase/GameTest.py
@@ -8,6 +8,18 @@ PyGame.CreateObject('Player.png')
 while PyGame.IsRunning:
     PyGame.FillScreen((200, 200, 200))
     
+    if PyGame.KeyPressed('Up'):
+        PyGame.Objects[0].Y -= int(325 * PyGame.DeltaTime)
+
+    if PyGame.KeyPressed('Down'):
+        PyGame.Objects[0].Y += int(325 * PyGame.DeltaTime)
+
+    if PyGame.KeyPressed('Left'):
+        PyGame.Objects[0].X -= int(325 * PyGame.DeltaTime)
+
+    if PyGame.KeyPressed('Right'):
+        PyGame.Objects[0].X += int(325 * PyGame.DeltaTime)
+
     for Object in PyGame.Objects:
         Object.Rect = Object.Image.get_rect(topleft=(Object.X, Object.Y))
         PyGame.DrawToScreen(Object.Image, Object.Rect)
